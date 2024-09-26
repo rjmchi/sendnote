23fecb97833ab274193220bf73b4b1d7187e640a
rjmchi <robert@rjmprogramming.com>
2024-09-25 20:28:09 -0600

    updates layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,9 @@
 
 <div>
     <h1 class="font-bold text-teal-600 3xl">Hello World!</h1>
+
+    <div class="bg-teal-400">
+        <x-button primary >Click Me</x-button>
+    </div>
 </div>
 </x-app-layout>
