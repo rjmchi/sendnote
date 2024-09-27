0c998fb89dbcc517acb157879f65d5378b75ad82
rjmchi <robert@rjmprogramming.com>
2024-09-27 09:21:21 -0600

    adds second paragraph to welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 
 <div>
     <h1 class="font-bold text-teal-600 3xl">Hello World!</h1>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Doloremque quasi quo nemo deserunt esse, nobis sed aperiam eligendi? Molestiae quas non doloremque vel perferendis ad, ratione totam adipisci laborum. Enim, incidunt veniam repellendus ea maiores officia aliquam blanditiis sit totam magni error cum suscipit sint cupiditate minus eveniet perferendis earum. Totam, praesentium porro corporis asperiores iste eaque nulla aliquam. Incidunt accusamus dolorum beatae temporibus. Eligendi ut in tempore aut. Nulla harum dignissimos fugiat tenetur facere commodi autem sapiente accusamus sequi, molestias, voluptate accusantium voluptates totam voluptas dicta voluptatem recusandae et, sed laudantium. Dolorum quod dolorem, dolore numquam harum quidem quae?</p>
-
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. </p>
+    <p class="mt-2">Doloremque quasi quo nemo deserunt esse, nobis sed aperiam eligendi? Molestiae quas non doloremque vel perferendis ad, ratione totam adipisci laborum. Enim, incidunt veniam repellendus ea maiores officia aliquam blanditiis sit totam magni error cum suscipit sint cupiditate minus eveniet perferendis earum. Totam, praesentium porro corporis asperiores iste eaque nulla aliquam. Incidunt accusamus dolorum beatae temporibus. Eligendi ut in tempore aut. Nulla harum dignissimos fugiat tenetur facere commodi autem sapiente accusamus sequi, molestias, voluptate accusantium voluptates totam voluptas dicta voluptatem recusandae et, sed laudantium. Dolorum quod dolorem, dolore numquam harum quidem quae?</p>
     <div class="bg-teal-400">
         <x-button primary >Click Me</x-button>
     </div>
